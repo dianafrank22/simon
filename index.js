73d65655abbf8c5671df0e44b11a5a18a4195c2f
Diana Frank <diana.frank22@gmail.com>
2015-12-17 09:50:26 -0500

    add array, randomchoice, and click function to js

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-$('document').ready(function(){
-
+$('document').ready(function() {
 
 var green = $('.green');
-var red= $('red');
-var blue = $('blue');
-var yellow =$('yellow');
+var red= $('.red');
+var blue = $('.blue');
+var yellow =$('.yellow');
 var lightGreen = $(".lightGreen");
+var start = $('.start_btn');
+
+
+var colors = [green, red, blue, yellow];
+
+
+var firstChoice = function(){
+	console.log(Math.floor(Math.random() * colors.length));
+
+};
 
 
 
-
-
-
-$(".green").on("click", function() {
-    $(this).toggleClass("lightGreen");
-}) 
+var beginGame = function(){
+		$(".start_btn").click(function(){
+			firstChoice();
+		});
 
 });
-// click function on button to start random selection of buttons to light up
 
-// animate to toggle class? 
 
-// math.floor(math.radom()* color.length)
 
-// var sequence = "";
-// // empty sequence to start
-// // after every round, if they match the sequence with clicks, then add onto last sequence
-// sequence += color
-	
-// });
+
+
+
+
+
